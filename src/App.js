24ae9f9c95ce9ce445615abdd86b0b6a04c84895
Chinweike99 <innocentakwolu1@gmail.com>
2024-09-28 23:19:23 +0100

    e-commerce:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Footer } from './Components/Footer/Footer';
+import { Header } from './Components/Header/Header';
+import { Hero } from './Components/Hero/Hero';
+import { Products } from './Components/Products/Products';
+import { Slider } from './Components/Slider/Slider';
+import { Testimonials } from './Components/Testimonials/Testimonials';
+import { Virtual } from './Components/Virtual/Virtual';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Hero />
+      <Slider />
+      <Virtual />
+      <Products />
+      <Testimonials />
+      <Footer />
     </div>
   );
 }
